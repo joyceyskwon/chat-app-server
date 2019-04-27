ed1b55ded43b2f270a185d46a5f0207e71eb44f0
joyceyskwon <joyceyskwon@gmail.com>
2019-04-27 03:17:17 -0400

    changed feed id

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TweetsController < ApplicationController
 
   def create
-    tweet = Tweet.new(content: params[:content], feed_id: 1)
+    tweet = Tweet.new(content: params[:content], feed_id: 5)
     if tweet.save
       # Broadcast the fact that this happened out to all the subscribers
       ActionCable.server.broadcast("feed_channel", tweet)
